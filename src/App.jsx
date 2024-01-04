ec8d68f0e602a3b6c4ad0f5de6f5fe5860a46d7a
Danila Albutov <danil.miheev2222@gmail.com>
2024-01-04 15:37:45 -0800

    Responsive Navbar section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <div className="container">
+          <Navbar/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Logo from "../../assets/website/logo.png"
+
+const Menu = [
+    {
+        id:1,
+        name: "Home",
+        link:"/#",
+    },
+    {
+        id:2,
+        name:"Best Seller",
+        link:"/#services",
+    },
+];
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="container py-3 sm:py-0"
+        >
+            <div className="flex justify-between items-center">
+                <div>
+                    <a href="#" className='font-bold text-2xl sm:text-3xl flex gap-2'>
+                        <img src={Logo} 
+                        alt=''
+                        className='w-10'
+                        >
+                        </img>
+                        Books
+                    </a>
+                </div>
+                <div>
+                    <ul className='items-center gap-4 hidden sm:flex'>
+                        {
+                            Menu.map((menu)=>(
+                                <li>
+                                    <a href={menu.link}
+                                    className='inline-block py-4 px-4 hover:text-primary duration-200'
+                                    >{menu.name}</a>
+                                </li>
+                            ))
+                        }
+                        {/* dropdown section */}
+                        <li className='group relative cursor-pointer'> 
+                            <a href="/#">
+                                Quick links
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
